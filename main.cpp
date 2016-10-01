9994ea1b706c98473b0ede19203d847822621aa3
celvira <clementh.elvira@gmail.com>
2016-10-01 22:34:16 +0200

    Adding CMakeLists.txt file, as well as a Matlab function and the cpp code calling the so called function

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+
+#include <stdio.h>  /* defines FILENAME_MAX */
+// #define WINDOWS  /* uncomment this line to use it for windows.*/
+#ifdef WINDOWS
+	#include <direct.h>
+	#define GetCurrentDir _getcwd
+#else
+	#include <unistd.h>
+	#define GetCurrentDir getcwd
+#endif
+
+#include "engine.h"
+
+#define  BUFSIZE 256
+
+std::string GetCurrentWorkingDir() {
+  char buff[FILENAME_MAX];
+  GetCurrentDir( buff, FILENAME_MAX );
+  std::string current_working_dir(buff);
+  return current_working_dir;
+}
+
+int main() {
+
+	Engine *ep;
+	mxArray *result = NULL;
+	char buffer[BUFSIZE+1];
+	buffer[BUFSIZE] = '\0';
+
+	/*
+	 * Call engOpen with a NULL string. This starts a MATLAB process
+     * on the current host using the command "matlab".
+	 */
+	if (!(ep = engOpen(""))) {
+		fprintf(stderr, "\nCan't start MATLAB engine\n");
+		return 1;
+	}
+
+	/*
+	 * Job
+	 */
+	engOutputBuffer(ep, buffer, BUFSIZE);
+
+	std::string addpath = "addpath('"
+		+ GetCurrentWorkingDir()
+		+ "')";
+	engEvalString(ep, addpath.c_str());
+
+	std::cout << "Current directory : ";
+	engEvalString(ep, "pwd");
+	printf("%s", buffer);
+
+	engEvalString(ep, "r = my_function();");
+	if ((result = engGetVariable(ep, "r")) == NULL)
+	   	std::cout << "Oops! things went wrong" << std::endl;
+	else {
+	   	double z = mxGetScalar(result);
+	   	std::cout << "output of function : " << z << std::endl;
+	}
+
+	/*
+	 * Free memory, close MATLAB engine and exit.
+	 */
+
+	std::cout  << "Done!" << std::endl;
+	mxDestroyArray(result);
+	engClose(ep);
+
+	return 0;
+}
+
+
+//export PATH=$PATH:/Applications/MATLAB_R2016a.app/bin
+
+
+
+
+
---
+++ my_function.m
@@ -0,0 +1,5 @@
+function t = my_function()
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+t = rand();
